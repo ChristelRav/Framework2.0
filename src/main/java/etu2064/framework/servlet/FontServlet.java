b7a9e8afd4bd3f37458b4f82fd6a0da285405358
ChristelRav <ravmihary@gmail.com>
2023-03-14 15:54:46 +0300

    sprint 2 : class Mapping + HashMap

--- src/main/java/etu2064/framework/servlet/FontServlet.java
+++ src/main/java/etu2064/framework/servlet/FontServlet.java
@@ -1,14 +1,17 @@
 package etu2064.framework.servlet;
 
+import etu2064.framework.Mapping;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 //@WebServlet(name = "FontServlet", value = "/FontServlet")
 public class FontServlet extends HttpServlet {
+    HashMap<String, Mapping> mappingUrls;
 //GET
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
