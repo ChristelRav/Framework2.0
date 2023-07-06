28d3f015c20e6ecf51c31eb01e449792ea87ac5f
ChristelRav <ravmihary@gmail.com>
2023-07-06 18:09:58 +0300

    sprint 6 send

--- doc/ModelView.java
+++ doc/ModelView.java
@@ -5,13 +5,11 @@ import java.util.HashMap;
 public class ModelView {
     String view;
     Map <String , Object> attribut = new HashMap<String , Object>();
-
+   
     public String getView() {return view;}                         public void setView(String view) {this.view = view;}
     public Map<String, Object> getAttribut() {return attribut;}    public void setAttribut(Map<String, Object> attribut) {this.attribut = attribut;}
     
     public ModelView(){}
-
-    ///ATTRIBUT
     public Map addItem(String cle, Object  valeur){
         this.getAttribut().put(cle, valeur);
         return this.getAttribut();
